49b1d2939b13337998ea64d3a347b325c7a014ce
Stefan Novitovic <rada.pet22@gmail.com>
2022-06-18 20:58:21 +0200

    added decorator to measure time required to produce all messages from file

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import ujson
 import gzip
 import time
 
+from decorators import mesaure_performance
 from kafka_producer import KafkaProducerClient
 from kafka_configuration import KafkaConfig
 
@@ -13,21 +14,25 @@ if __name__ == '__main__':
     try:
         kafka_producer_instance = KafkaProducerClient().get_producer()
 
-        with gzip.open('stream.jsonl.gz', 'rt') as data:
-            for record in data:
-                try:
-                    kafka_producer_instance.produce(
-                        KafkaConfig().kafka_service_output_topic,
-                        ujson.dumps(record)
-                    )
-                    kafka_producer_instance.poll(0)
-                except BufferError as e:
-                    logging.info("Queue is full. Waiting for the free space!")
-                    kafka_producer_instance.poll(20)
-                    kafka_producer_instance.produce(
-                        KafkaConfig().kafka_service_output_topic,
-                        ujson.dumps(record)
-                    )
-        kafka_producer_instance.flush()
+        @mesaure_performance
+        def produce_messages():
+            with gzip.open('stream.jsonl.gz', 'rt') as data:
+                for record in data:
+                    try:
+                        kafka_producer_instance.produce(
+                            KafkaConfig().kafka_service_output_topic,
+                            ujson.dumps(record)
+                        )
+                        kafka_producer_instance.poll(0)
+                    except BufferError as e:
+                        logging.info("Queue is full. Waiting for the free space!")
+                        kafka_producer_instance.poll(20)
+                        kafka_producer_instance.produce(
+                            KafkaConfig().kafka_service_output_topic,
+                            ujson.dumps(record)
+                        )
+            kafka_producer_instance.flush()
+
+        produce_messages()
     except Exception as e:
         logging.info(str(e))
